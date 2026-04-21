683dd6d28cdd483f2d745dc3f8b6212e219515e7
Ola Yousof <olala.araby131@gmail.com>
2026-04-22 01:58:27 +0400

    Delete eg_property_management directory

--- eg_property_management/models/account_move.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class AccountMove(models.Model):
-    _inherit = "account.move"
-
-    property_id = fields.Many2one(comodel_name="property.detail", string="Property")
-    rent_contract_id = fields.Many2one(comodel_name='rent.contract', string='Contract')
-    sale_contract_id = fields.Many2one(comodel_name='sale.contract', string='Contract')

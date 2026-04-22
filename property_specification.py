4c8d8bec2ae4c9ab76ee089a3690c25eda615ff5
Ola Yousof <olala.araby131@gmail.com>
2026-04-22 05:34:11 +0400

    Delete property_specification.py

--- property_specification.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class PropertySection(models.Model):
-    _name = "property.section"
-    _description = "Property Section"
-
-    name = fields.Char(string='Name')

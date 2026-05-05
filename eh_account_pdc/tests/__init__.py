8cbb634f55eeecfe69dd8bd6ee1bb99ae6ab669f
Ola Yousof <olala.araby131@gmail.com>
2026-05-05 09:49:34 +0400

    Delete eh_account_pdc directory

--- eh_account_pdc/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# Phase 1 test plan:
-#
-# Functional unit tests:
-from . import test_cheque_book
-from . import test_cheque_lifecycle
-from . import test_bounce_replace
--- eh_account_pdc/wizards/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- encoding: utf-8 -*-
-from . import bounce_cheque_wizard
-from . import replace_cheque_wizard
